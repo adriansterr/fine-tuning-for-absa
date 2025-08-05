2e272504b32ed720a13d78d4a42abe74b0761a2d
Adrian Sterr <53040453+adriansterr@users.noreply.github.com>
2025-08-05 15:59:47 +0200

    eval all models in folder

--- baselines/llm/llama_eval.py
+++ baselines/llm/llama_eval.py
@@ -1,10 +1,13 @@
+from unittest import result
 from unsloth import FastLanguageModel
 import torch
 from tqdm import tqdm
 import sys, os
+import argparse
 sys.path.append(os.path.abspath('./src/utils/'))
+import glob
 
-from evaluation import extractAspects, convertLabels, createResults, subset_recall
+from evaluation import extractAspects, convertLabels, createResults
 from preprocessing import loadDataset, createPrompts
 from config import Config
 
@@ -22,18 +25,18 @@ def evaluate_model(model, tokenizer, config, prompts_test, ground_truth_labels, 
             )
         output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
         predictions.append(output_text)
-        print(f"Prompt: {prompt}\nPrediction: {output_text}\n")
+        # print(f"Prompt: {prompt}\nPrediction: {output_text}\n")
 
     processed_predictions = [extractAspects(p, config.task, config.prompt_style) for p in predictions]
     processed_ground_truths = [extractAspects(gt, config.task, config.prompt_style) for gt in ground_truth_labels]
 
-    print("Processed Ground Truths:", processed_ground_truths, "\n")
+    # print("Processed Ground Truths:", processed_ground_truths, "\n")
 
     gold_labels, _ = convertLabels(processed_ground_truths, config.task, label_space)
     pred_labels, false_predictions = convertLabels(processed_predictions, config.task, label_space)
     
-    print("Gold Labels:", gold_labels, "\n")
-    print("Predicted Labels:", pred_labels, "\n")
+    # print("Gold Labels:", gold_labels, "\n")
+    # print("Predicted Labels:", pred_labels, "\n")
 
     results_asp, results_asp_pol, results_pairs, results_pol, results_phrases, results_subset_recall = createResults(pred_labels, gold_labels, label_space, config.task)
 
@@ -60,22 +63,31 @@ def evaluate_model(model, tokenizer, config, prompts_test, ground_truth_labels, 
     print("Subset recall:", results_subset_recall)
     
     
-if __name__ == "__main__":
-    model_name = "D:/Uni/Masterarbeit Code/test/mergekit/merges/trained/llama/meta_llama_full_precision_sauerkraut/linear/50_50"
+if __name__ == "__main__":    
+    base_model_dir = "D:/Uni/Masterarbeit Code/test/mergekit/merges/trained/llama/meta_llama_full_precision_sauerkraut/evaluate"
+    model_dirs = [f for f in glob.glob(f"{base_model_dir}/*") if os.path.isdir(f)]
 
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name,
-        max_seq_length=2048,
-        device_map="cuda" 
-    )
-    FastLanguageModel.for_inference(model)
+    for model_name in model_dirs:
+        print(f"Evaluating model: {model_name}")
+        results_path = os.path.join("results/merges/meta_llama_full_precision_sauerkraut/finished_evaluation", os.path.basename(model_name))
+        os.makedirs(results_path, exist_ok=True)
 
-    config = Config()
-    config.dataset = 'GERestaurant'
-    df_train, df_test, label_space = loadDataset(config.data_path, config.dataset, config.low_resource_setting, config.task, config.split, config.original_split)
-    
-    # Limit test set to 20 entries for testing purposes
-    df_test = df_test.iloc[:5]
-    
-    prompts_train, prompts_test, ground_truth_labels = createPrompts(df_train, df_test, config, eos_token=tokenizer.eos_token)
-    evaluate_model(model, tokenizer, config, prompts_test, ground_truth_labels, label_space, results_path="results/llama/meta_llama_full_precision_sauerkraut/linear/")
+        model, tokenizer = FastLanguageModel.from_pretrained(
+            model_name,
+            max_seq_length=2048,
+            device_map="cuda"
+        )
+        FastLanguageModel.for_inference(model)
+
+        config = Config()
+        config.dataset = 'GERestaurant'
+        config.lang = 'ger'
+
+        df_train, df_test, label_space = loadDataset(config.data_path, config.dataset, config.low_resource_setting, config.task, config.split, config.original_split)
+        prompts_train, prompts_test, ground_truth_labels = createPrompts(df_train, df_test, config, eos_token=tokenizer.eos_token)
+        evaluate_model(model, tokenizer, config, prompts_test, ground_truth_labels, label_space, results_path=results_path)
+
+        # Free up VRAM
+        del model
+        del tokenizer
+        torch.cuda.empty_cache()
--- baselines/llm/llama_interference.py
+++ baselines/llm/llama_interference.py
@@ -8,7 +8,7 @@ sys.path.append(utils)
 from preprocessing import createPromptText
 
 
-model_name = "D:/Uni/Masterarbeit Code/test/mergekit/merges/trained/llama/meta_llama_full_precision_sauerkraut/linear"
+model_name = "finetuned_models/meta_llama_full_colab_remerge_2/"
 
 # FastLanguageModel von unsloth
 model, tokenizer = FastLanguageModel.from_pretrained(
@@ -19,11 +19,11 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 FastLanguageModel.for_inference(model)
 
 # base_dir = os.path.dirname(os.path.abspath(__file__))
-with open("src/utils/prompts_rest16.json", encoding='utf-8') as json_prompts:
+with open("src/utils/prompts_GERestaurant.json", encoding='utf-8') as json_prompts:
     prompt_templates = json.load(json_prompts)
 
 prompt, _ = createPromptText(
-    lang='en',
+    lang='ger',
     prompt_templates=prompt_templates,
     prompt_style='basic',
     example_text="Das Essen war sehr lecker, aber der Service war schlecht.", 
@@ -40,4 +40,4 @@ inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
 
 text_streamer = TextStreamer(tokenizer)
 print("Generating text...\n")
-_ = model.generate(input_ids=inputs["input_ids"], streamer=text_streamer, max_new_tokens=256)
+_ = model.generate(input_ids=inputs["input_ids"], streamer=text_streamer, max_new_tokens=128)
